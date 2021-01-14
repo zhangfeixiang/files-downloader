07368003711a53f5e0746e75493545eb7c72c769
小鱼（张飞翔） <314717701@qq.com>
2021-01-14 18:26:32 +0800

    删除默认目录

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -94,7 +94,7 @@ export default {
       showDownloadList: false,
       list: [],
       urls: ``,
-      outputDir: "C:\\Users\\zfx\\Desktop\\test",
+      outputDir: "",
     };
   },
   computed: {
